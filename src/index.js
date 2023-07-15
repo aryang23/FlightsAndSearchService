a28c1b0cc63742358942dc6b5ad69e35a3dcead2
Aryan Grover <aryang23@Aryans-MacBook-Pro.local>
2023-07-16 02:12:07 +0530

    Added City Controller and Routing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
-const CityRepository = require('./repository/city-repository');
-const app = express();
 
-const setupAndStartServer = async() => {
-    const app = express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+const ApiRoutes = require("./routes/index");
 
-    app.listen(PORT, ()=>{
-        console.log(`Server starting at ${PORT}`);
-        const repo = new CityRepository();
-        repo.createCity({name: "New Delhi"});
-    });
-}
+const setupAndStartServer = async () => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", ApiRoutes);
+
+  app.listen(PORT, () => {
+    console.log(`Server starting at ${PORT}`);
+  });
+};
 
 setupAndStartServer();
